319363fd8aa46dacc15ff125e0c57cab41898196
Hayley Bech <hayleybech@gmail.com>
2025-10-22 13:30:03 +0800

    Hide home page for now

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+//Route::get('/', function () {
+//    return Inertia::render('welcome');
+//})->name('home');
 
-Route::get('/cowwwch', function () {
+Route::get('/', function () {
     return Inertia::render('games/snakes');
 })->name('snake');
 
